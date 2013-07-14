bc51f263f839d1ccc65633cade3187c6ad87e3a1
Masayuki Ono <mono0926@gmail.com>
2013-07-14 15:17:44 +0900

    added mn

---
+++ CocoaCommonLib/View/Utils/UIViewController+MN.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+MN.h
+//  CocoaCommonLib
+//
+//  Created by mono on 7/12/13.
+//  Copyright (c) 2013 mono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+@interface UIViewController (MN)
+
+@end
---
+++ CocoaCommonLib/View/Utils/UIViewController+MN.m
@@ -0,0 +1,14 @@
+//
+//  UIViewController+MN.m
+//  CocoaCommonLib
+//
+//  Created by mono on 7/12/13.
+//  Copyright (c) 2013 mono. All rights reserved.
+//
+
+#import "UIViewController+MN.h"
+#import "Syringe.h"
+
+@implementation UIViewController (MN)
+
+@end
